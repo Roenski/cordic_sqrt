a4db4350325a543b2dd49c6d4d068c7881d19e53
Aleksi Korsman <aleksi.korsman@aalto.fi>
2022-11-19 12:19:40 +0200

    Add cordic module

---
+++ src/main/scala/cordicSqrt/CORDICSqrt.scala
@@ -0,0 +1,33 @@
+package cordicSqrt
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental._
+
+/**
+  * One iteration of CORDIC for sqrt
+  *
+  */
+class CORDICSqrt(width: Int, iterations: Int) extends Module {
+  val in = IO(Input(new Bundle {
+    val xn   = UInt(width.W)
+    val yn   = UInt(width.W)
+    val iter = UInt((math.log(iterations)/math.log(2)).toInt.W)
+  }))
+  val out = IO(Output(new Bundle {
+    val xn1 = UInt(width.W)
+    val yn1 = UInt(width.W)
+  }))
+
+  val xtemp = in.xn >> in.iter
+  val ytemp = in.yn >> in.iter
+
+  when (in.yn(width-1) === 1.U) {
+    out.xn1 := in.xn + ytemp 
+    out.yn1 := in.yn + xtemp
+  } .otherwise {
+    out.xn1 := in.xn - ytemp 
+    out.yn1 := in.yn - xtemp
+  }
+
+}
