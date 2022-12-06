2ebc263754042415059ce7a02b0d764ec904038c
Aleksi Korsman <aleksi.korsman@aalto.fi>
2022-12-06 09:27:40 +0200

    Rename to camelcase

--- src/main/scala/cordicSqrt/Preprocessor.scala
+++ src/main/scala/cordicSqrt/Preprocessor.scala
@@ -16,10 +16,10 @@ class PreProcessor extends Module {
     })
 
     val out = Output(new Bundle {
-      val mant       = UInt(52.W)
-      val mant_round = UInt(1.W)
-      val expo       = UInt(11.W)
-      val data       = ValidIO(CORDICSqrtOutput())
+      val mantissa         = UInt(52.W)
+      val mantissaRoundBit = UInt(1.W)
+      val expo             = UInt(11.W)
+      val data             = ValidIO(CORDICSqrtOutput())
     })
 
   })
@@ -28,94 +28,94 @@ class PreProcessor extends Module {
 
   val exponent = io.in.data(62, 52)
 
-  val mant_with_hidden = Cat(
+  val mantWithHidden = Cat(
     Mux(exponent === bias, 0.U, 1.U),
     io.in.data(51, 0)
   )
 
-  val mantissa = io.in.data(51, 0)
-  val sign     = io.in.data(63)
-  val mant_rnd = WireDefault(0.U(1.W))
-  val new_expo = WireDefault(0.U(11.W))
-  val new_mant = WireDefault(0.U(52.W))
+  val mantissa         = io.in.data(51, 0)
+  val sign             = io.in.data(63)
+  val mantissaRoundBit = WireDefault(0.U(1.W))
+  val newExponent      = WireDefault(0.U(11.W))
+  val newMantissa      = WireDefault(1.U(52.W))
 
-  io.out.mant       := new_mant
-  io.out.expo       := new_expo
-  io.out.mant_round := mant_rnd
+  io.out.mantissa         := newMantissa
+  io.out.expo             := newExponent
+  io.out.mantissaRoundBit := mantissaRoundBit
 
   // Calculate new exponent
   // and adjust mantissa when necessary
   when(exponent === bias) {
-    new_expo := exponent
+    newExponent := exponent
   }.elsewhen(exponent(0)) {
     // Exponent is odd (or even, after subtracting bias)
-    new_expo := (exponent >> 1) + (bias >> 1) + 1.U
+    newExponent := (exponent >> 1) + (bias >> 1) + 1.U
   }.otherwise {
     // Exponent is even (or odd, after subtracting bias)
     // Both exponent and bias have an LSB, thus we can forget them
     // and divide by 2 by shifting right, and then just add 1
-    new_expo := ((exponent + 1.U) >> 1) + (bias >> 1) + 1.U
-    new_mant := mant_with_hidden >> 1
-    mant_rnd := mant_with_hidden(0)
+    newExponent      := ((exponent + 1.U) >> 1) + (bias >> 1) + 1.U
+    newMantissa      := mantWithHidden >> 2
+    mantissaRoundBit := mantWithHidden(0)
   }
 
   // Special cases
   // If one of them matches, valid is set true
-  val valid          = WireDefault(false.B)
-  val sign_valid     = WireDefault(0.U(1.W))
-  val mantissa_valid = WireDefault(0.U(52.W))
-  val exponent_valid = WireDefault(0.U(11.W))
-  val fflags_valid   = VecInit.fill(5) { false.B }
+  val valid         = WireDefault(false.B)
+  val signValid     = WireDefault(0.U(1.W))
+  val mantissaValid = WireDefault(0.U(52.W))
+  val exponentValid = WireDefault(0.U(11.W))
+  val fflagsValid   = VecInit.fill(5) { false.B }
 
   val data_valid = Mux(
     valid,
-    Cat(sign_valid, exponent_valid, mantissa_valid),
+    Cat(signValid, exponentValid, mantissaValid),
     0.U(64.W)
   )
 
   io.out.data.bits.data   := data_valid
-  io.out.data.bits.fflags := fflags_valid.asUInt
+  io.out.data.bits.fflags := fflagsValid.asUInt
   io.out.data.valid       := valid
 
-  val is_zero = (mantissa === 0.U && exponent === 0.U)
-  val is_nan  = exponent.andR && mantissa.orR
-  val is_qnan = (is_nan && mantissa(51))
-  val is_snan = (is_nan && !mantissa(51))
-  val is_inf  = exponent.andR && !mantissa.orR
+  val isZero = (mantissa === 0.U && exponent === 0.U)
+  val isNan  = exponent.andR && mantissa.orR
+  val isQnan = (isNan && mantissa(51))
+  val isSnan = (isNan && !mantissa(51))
+  val isInf  = exponent.andR && !mantissa.orR
 
-  when(is_zero) {
+  when(isZero) {
     // no flags
-    sign_valid     := sign
-    mantissa_valid := mantissa
-    exponent_valid := exponent
-    valid          := true.B
-  }.elsewhen(is_qnan) {
+    signValid     := sign
+    mantissaValid := mantissa
+    exponentValid := exponent
+    valid         := true.B
+  }.elsewhen(isQnan) {
     // no flags
-    sign_valid     := sign
-    mantissa_valid := mantissa
-    exponent_valid := exponent
-    valid          := true.B
-  }.elsewhen(is_snan) {
+    signValid     := sign
+    mantissaValid := mantissa
+    exponentValid := exponent
+    valid         := true.B
+  }.elsewhen(isSnan) {
     // invalid flag
-    sign_valid                             := sign
-    mantissa_valid                         := mantissa
-    exponent_valid                         := exponent
-    valid                                  := true.B
-    fflags_valid(FaultFlag.invalid.asUInt) := true.B
+    signValid                             := sign
+    mantissaValid                         := mantissa
+    exponentValid                         := exponent
+    valid                                 := true.B
+    fflagsValid(FaultFlag.invalid.asUInt) := true.B
   }.elsewhen(sign) {
     // output a qNan
     // and an invalid flag
-    sign_valid                             := sign
-    mantissa_valid                         := Cat(1.U, mantissa(50, 0))
-    exponent_valid                         := Fill(11, 1.U)
-    valid                                  := true.B
-    fflags_valid(FaultFlag.invalid.asUInt) := true.B
-  }.elsewhen(is_inf) {
+    signValid                             := sign
+    mantissaValid                         := Cat(1.U, mantissa(50, 0))
+    exponentValid                         := Fill(11, 1.U)
+    valid                                 := true.B
+    fflagsValid(FaultFlag.invalid.asUInt) := true.B
+  }.elsewhen(isInf) {
     // no flags
-    sign_valid     := sign
-    mantissa_valid := mantissa
-    exponent_valid := exponent
-    valid          := true.B
+    signValid     := sign
+    mantissaValid := mantissa
+    exponentValid := exponent
+    valid         := true.B
   }
 
 }
